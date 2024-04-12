c630efc50f923e64fb3e941570b2420eba9e1d05
Your Name <you@example.com>
2024-04-12 22:13:47 +0000

    update

---
+++ sequence_tp_benchmark.py
@@ -0,0 +1,349 @@
+import argparse
+import os
+import time
+
+import tempfile
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+from torch.optim import AdamW
+
+from utils import load, load_jsonl, load_data
+from datasets import load_dataset, load_from_disk
+
+from transformers import TrainingArguments, TextDataset, DataCollatorForLanguageModeling
+
+import numpy as np
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed.fsdp import FullyShardedDataParallel
+import torch.multiprocessing as mp
+import torch.distributed as dist
+
+import transformers
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Generator,
+    Iterator,
+    List,
+    Mapping,
+    NamedTuple,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    Union,
+)
+
+RPC_PORT = 29501
+
+def init_random_seed(seed: int):
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+
+def ensure_divisibility(numerator: int, denominator: int) -> None:
+    """Ensure that numerator is divisible by the denominator."""
+    assert numerator % denominator == 0, "{} is not divisible by {}".format(numerator, denominator)
+
+def divide(numerator, denominator):
+    """Ensure that numerator is divisible by the denominator and return
+    the division value."""
+    ensure_divisibility(numerator, denominator)
+    return numerator // denominator
+
+def split_tensor(
+    tensor: torch.Tensor, num_partitions: int, contiguous_split_chunks: bool = False, dim: int = -1
+) -> List[torch.Tensor]:
+    """ Split a tensor along its last dimension.
+
+        Arguments:
+            tensor: input tensor.
+            num_partitions: number of partitions to split the tensor
+            contiguous_split_chunks: If True, make each chunk contiguous
+                                     in memory.
+
+        Returns:
+            A list of Tensors
+    """
+    # Get the size and dimension.
+    dim_size = divide(tensor.size()[dim], num_partitions)
+    # Split.
+    tensor_list = torch.split(tensor, dim_size, dim=dim)
+    # Note: torch.split does not create contiguous tensors by default.
+    if contiguous_split_chunks:
+        return tuple(chunk.contiguous() for chunk in tensor_list)
+
+    return tensor_list
+
+def _gather_along_first_dim(input_):
+    """Gather tensors and concatinate along the first dimension."""
+
+    group = torch.distributed.distributed_c10d._get_default_group()
+    world_size = torch.distributed.get_world_size(group=group)
+    # Bypass the function if we are using only 1 GPU.
+    if world_size == 1:
+        return input_
+
+    dim_size = list(input_.size())
+    dim_size[1] = dim_size[1] * world_size
+
+    output = torch.empty(dim_size, dtype=input_.dtype, device=torch.cuda.current_device())
+    torch.distributed._all_gather_base(
+        output, input_.contiguous(), group=group
+    )
+
+    return output
+
+def _reduce_scatter_along_first_dim(input_):
+    """Reduce-scatter the input tensor across model parallel group."""
+    group = torch.distributed.distributed_c10d._get_default_group()
+    world_size = torch.distributed.get_world_size(group=group)
+    # Bypass the function if we are using only 1 GPU.
+    if world_size == 1:
+        return input_
+
+    dim_size = list(input_.size())
+    assert (
+        dim_size[1] % world_size == 0
+    ), "First dimension of the tensor should be divisible by tensor parallel size"
+
+    dim_size[1] = dim_size[1] // world_size
+
+    output = torch.empty(dim_size, dtype=input_.dtype, device=torch.cuda.current_device())
+    torch.distributed._reduce_scatter_base(
+        output, input_.contiguous(), group=group
+    )
+    return output
+    
+    
+class _GatherFromSequenceParallelRegion(torch.autograd.Function):
+    """Gather the input from sequence parallel region and concatinate."""
+
+    @staticmethod
+    def symbolic(graph, input_, tensor_parallel_output_grad=True):
+        return _gather_along_first_dim(input_)
+
+    @staticmethod
+    def forward(ctx, input_, tensor_parallel_output_grad=True):
+        ctx.tensor_parallel_output_grad = tensor_parallel_output_grad
+        return _gather_along_first_dim(input_)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        tensor_parallel_output_grad = ctx.tensor_parallel_output_grad
+        return _reduce_scatter_along_first_dim(grad_output), None
+        
+class _ReduceScatterToSequenceParallelRegion(torch.autograd.Function):
+    """Reduce scatter the input from the model parallel region."""
+
+    @staticmethod
+    def symbolic(graph, input_):
+        return _reduce_scatter_along_first_dim(input_)
+
+    @staticmethod
+    def forward(ctx, input_):
+        return _reduce_scatter_along_first_dim(input_)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return _gather_along_first_dim(grad_output)
+
+class SequenceWarpper(nn.Module):
+    def __init__(
+        self,
+        module: nn.Module,
+        group: Optional[Any] = None,
+    ):
+        super().__init__()
+        self.module = module
+        self.group = group
+        
+    def forward(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+
+        inputs = _GatherFromSequenceParallelRegion.apply(args[0])
+        outputs = self.module(inputs, **kwargs)
+        if isinstance(outputs, tuple):
+            outputs = list(outputs)
+            outputs[0] = _ReduceScatterToSequenceParallelRegion.apply(outputs[0])
+            outputs = tuple(outputs)
+        else:
+            outputs = _ReduceScatterToSequenceParallelRegion.apply(outputs)
+            
+        return outputs
+        
+class SequenceParallel(nn.Module):
+    def __init__(
+        self,
+        module,
+        group: Optional[Any] = None):
+        super().__init__()
+        
+        self.module = module
+        self.group = group if group is not None else dist.group.WORLD
+        self.world_size = dist.get_world_size(self.group)
+        self.rank = dist.get_rank(self.group)
+        
+        self.RecursiveVisit('module', self.module, self.module)
+        
+    def RecursiveVisit(self, name, module, upper_module):
+        """
+        Recursively replace layers in the module with the custom layer.
+        
+        Args:
+        - module (nn.Module): The module (or model) to modify.
+        - custom_layer_class (nn.Module): The custom layer class to replace with.
+        """
+            
+        has_parameters = any(isinstance(param, nn.Parameter) for param in module.parameters())
+        has_child = any(isinstance(child, nn.Module) for child in module.children())
+        is_MultiheadAttention = isinstance(module, transformers.models.llama.modeling_llama.LlamaAttention) or isinstance(module, transformers.models.llama.modeling_llama.LlamaMLP) or isinstance(module, transformers.models.gpt_neo.modeling_gpt_neo.GPTNeoMLP) or isinstance(module, transformers.models.gpt2.modeling_gpt2.GPT2Attention) or isinstance(module, transformers.models.gpt2.modeling_gpt2.GPT2MLP)
+        is_linear = nn.Linear
+
+        if has_child and not is_MultiheadAttention:
+            for name, child in module.named_children():
+                self.RecursiveVisit(name, child, module)
+        else:
+            if isinstance(module, transformers.models.gpt2.modeling_gpt2.GPT2Attention):
+            
+                if module.c_attn is not None:
+                    module.c_attn.weight = nn.Parameter(split_tensor(module.c_attn.weight, self.world_size, dim=1)[self.rank])
+                    if module.c_attn.bias is not None:
+                        module.c_attn.bias = nn.Parameter(split_tensor(module.c_attn.bias, self.world_size, dim=0)[self.rank])
+                        module.c_attn.nf = module.c_attn.nf // self.world_size
+                if hasattr(module, 'q_attn'):
+                    module.q_attn.weight = nn.Parameter(split_tensor(module.q_attn.weight, self.world_size, dim=1)[self.rank])
+                    if module.q_attn.bias is not None:
+                        module.q_attn.bias = nn.Parameter(split_tensor(module.q_attn.bias, self.world_size, dim=0)[self.rank])
+                        module.q_attn.nf = module.q_attn.nf // self.world_size
+                module.num_heads = module.num_heads // self.world_size
+                module.split_size = module.split_size // self.world_size
+
+                module.c_proj.weight = nn.Parameter(split_tensor(module.c_proj.weight, self.world_size, dim=0)[self.rank])
+                module = SequenceWarpper(module, self.group)
+                setattr(upper_module, name, module)
+            elif isinstance(module, transformers.models.gpt2.modeling_gpt2.GPT2MLP):
+                module.c_fc.weight = nn.Parameter(split_tensor(module.c_fc.weight, self.world_size, dim=1)[self.rank])
+                if module.c_fc.bias is not None:
+                    module.c_fc.bias = nn.Parameter(split_tensor(module.c_fc.bias, self.world_size, dim=0)[self.rank])
+                module.c_fc.nf = module.c_fc.nf // self.world_size
+                    
+                module.c_proj.weight = nn.Parameter(split_tensor(module.c_proj.weight, self.world_size, dim=0)[self.rank])
+                module = SequenceWarpper(module, self.group)
+                setattr(upper_module, name, module)
+            
+    def forward(self, *args: Any, **kwargs: Any) -> torch.Tensor:
+        outputs = self.module(*args, **kwargs)
+        return outputs
+
+def benchmark_dp(rank, args, world_size):
+    """Benchmark a given model using a single process and multiple devices."""
+    init_method_pgroup = "tcp://localhost:{}".format(RPC_PORT)
+    torch.distributed.init_process_group(
+        backend="nccl", rank=rank, world_size=world_size, init_method=init_method_pgroup
+    )
+
+    torch.cuda.set_device(rank)
+    init_random_seed(0)
+    
+    # Specify the pretrained model name or path
+    model_name = args.model_name
+    
+    # Load the tokenizer and pretrained model
+    model, tokenizer = load(model_name)
+    
+    # Move the model to GPU(s)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+    model = SequenceParallel(model)
+    print(model)
+    
+    # Random data generator dataset class
+    class RandomDataGenerator(Dataset):
+        def __init__(self, tokenizer, num_samples, max_length):
+            self.tokenizer = tokenizer
+            self.num_samples = num_samples
+            self.max_length = max_length
+            self.vocab_size = len(tokenizer)  # Get the size of the tokenizer's vocabulary
+    
+        def __len__(self):
+            return self.num_samples
+        
+        def __getitem__(self, idx):
+            random_sequence = np.random.randint(low=0, high=self.vocab_size, size=(self.max_length,))
+            return torch.tensor(random_sequence)  
+    
+    # Instantiate the dataset
+    num_samples = args.num_samples  # Number of random samples you want to generate
+    max_length = args.max_length  # Maximum length of the sequence
+    dataset = RandomDataGenerator(tokenizer, num_samples, max_length)
+    
+    # DataLoader
+    data_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    
+    # Set up the optimizer
+    # Training loop
+    num_epochs = 3
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        model.train()
+        total_loss = 0
+    
+        for batch in data_loader:
+            inputs = batch.to(device)
+            outputs = model(input_ids=inputs, labels=inputs)
+            loss = outputs.loss
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            total_loss += loss.item()
+    
+        avg_loss = total_loss / len(data_loader)
+        epoch_time = time.time() - start_time
+        print(f"Epoch {epoch+1}/{num_epochs} - Training Loss: {avg_loss:.4f} - Time: {epoch_time:.2f} seconds")
+
+
+    print(
+        "Peak allocated bytes on cuda:{}: {:4f}GB".format(
+            dist.get_rank(), torch.cuda.memory_stats(dist.get_rank())["allocated_bytes.all.peak"] / 2**30
+        )
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name", type=str, default="openai-community/gpt2"
+    )
+    parser.add_argument(
+        "--dataset_name", type=str, default="yelp_review_full"
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=1
+    )
+    parser.add_argument(
+        "--num_samples", type=int, default=10
+    )
+    parser.add_argument(
+        "--max_length", type=int, default=512
+    )
+    parser.add_argument("--data_root", type=str, default="data/")
+    args = parser.parse_args()
+    
+    print(f"Running DP benchmark with args: {args}")
+    num_devices = torch.cuda.device_count() if torch.cuda.is_available() else 1
+    print(torch.cuda.device_count())
+
+    mp.spawn(
+        benchmark_dp,
+        args=(args, num_devices),
+        nprocs=num_devices,
+        join=True,
+    )
